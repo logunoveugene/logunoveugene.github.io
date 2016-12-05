b676871153ee676742b5375190baba879580ef31
logunoveugene <logunov.design@yandex.ru>
2016-12-05 23:17:02 +1000

    ываыва

--- dt/js/app.js
+++ dt/js/app.js
@@ -1,1 +1,9 @@
 $(document).foundation()
+$(".site-register-check").hide();
+$("#c2").click(function() {
+    if($(this).is(":checked")) {
+        $(".site-register-check").show();
+    } else {
+        $(".site-register-check").hide();
+    }
+});
