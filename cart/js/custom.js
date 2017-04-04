8b4cdd7ff554f2f66702af81c14fab5aeae6ef0d
logunoveugene <logunov.design@yandex.ru>
2017-04-04 15:19:01 +1000

    sdfsdf

---
+++ cart/js/custom.js
@@ -0,0 +1,45 @@
+ $(function () {
+      $('[data-toggle="tooltip"]').tooltip({
+        
+        trigger: 'click',
+        constraints: [
+        {
+          to: 'scrollParent',
+          pin: true
+        },
+        ]}
+        )
+    });
+
+    jQuery('.quantity').each(function() {
+      var spinner = jQuery(this),
+      input = spinner.find('input[type="number"]'),
+      btnUp = spinner.find('.quantity-up'),
+      btnDown = spinner.find('.quantity-down'),
+      min = input.attr('min'),
+      max = input.attr('max');
+
+      btnUp.click(function() {
+        var oldValue = parseFloat(input.val());
+        if (oldValue >= max || input.attr('disabled')=="disabled" )  {
+          var newVal = oldValue;
+        } else {
+          var newVal = oldValue + 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+      });
+
+      btnDown.click(function() {
+        var oldValue = parseFloat(input.val());
+        if (oldValue <= min || input.attr('disabled')=="disabled") {
+          var newVal = oldValue;
+        } else {
+          var newVal = oldValue - 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+      });
+
+    });
+  //# sourceURL=pen.js
