58f6c4b78d595fc7e0afa7813ac97e18604745a5
logunoveugene <logunov.design@yandex.ru>
2017-05-17 19:12:57 +1000

    sdfsdf

--- dtcart2/js/custom.js
+++ dtcart2/js/custom.js
@@ -1,274 +1,250 @@
- $(function () {
-  $('[data-toggle="tooltip"]').tooltip({
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip({
 
-    trigger: 'click',
-    constraints: [
-    {
-      to: 'scrollParent',
-      pin: true
-    },
-    ]}
-    )
+        trigger: 'click',
+        constraints: [{
+            to: 'scrollParent',
+            pin: true
+        }, ]
+    })
 });
 
- $('.quantity').each(function() {
-  var spinner = jQuery(this),
-  input = spinner.find('input[type="number"]'),
-  btnUp = spinner.find('.quantity-up'),
-  btnDown = spinner.find('.quantity-down'),
-  min = input.attr('min'),
-  max = input.attr('max');
+$('.quantity').each(function() {
+    var spinner = jQuery(this)
+      , input = spinner.find('input[type="number"]')
+      , btnUp = spinner.find('.quantity-up')
+      , btnDown = spinner.find('.quantity-down')
+      , min = input.attr('min')
+      , max = input.attr('max');
 
-  btnUp.click(function() {
-    var oldValue = parseFloat(input.val());
-    if (oldValue >= max || input.attr('disabled')=="disabled" )  {
-      var newVal = oldValue;
+    btnUp.click(function() {
+        var oldValue = parseFloat(input.val());
+        if (oldValue >= max || input.attr('disabled') == "disabled") {
+            var newVal = oldValue;
+        } else {
+            var newVal = oldValue + 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+    });
+
+    btnDown.click(function() {
+        var oldValue = parseFloat(input.val());
+        if (oldValue <= min || input.attr('disabled') == "disabled") {
+            var newVal = oldValue;
+        } else {
+            var newVal = oldValue - 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+    });
+
+});
+
+$('input[name=radio]').on('change', function() {
+    if ($("#radio1").is(':checked')) {
+        $('#collapseExample1').collapse('show');
+        $('div[data-quantity=radio1]').addClass('collapse-alert-warning');
     } else {
-      var newVal = oldValue + 1;
+        $('#collapseExample1').collapse('hide');
+        $('div[data-quantity=radio1]').removeClass('collapse-alert-warning');
     }
-    spinner.find("input").val(newVal);
-    spinner.find("input").trigger("change");
-  });
+    if ($("#radio2").is(':checked')) {
+        $('#collapseExample2').collapse('show');
+        $('div[data-quantity=radio2]').addClass('collapse-alert-warning');
 
-  btnDown.click(function() {
-    var oldValue = parseFloat(input.val());
-    if (oldValue <= min || input.attr('disabled')=="disabled") {
-      var newVal = oldValue;
     } else {
-      var newVal = oldValue - 1;
+        $('#collapseExample2').collapse('hide');
+        $('div[data-quantity=radio2]').removeClass('collapse-alert-warning');
     }
-    spinner.find("input").val(newVal);
-    spinner.find("input").trigger("change");
-  });
+    if ($("#radio3").is(':checked')) {
+        $('#collapseExample3').collapse('show');
+        $('div[data-quantity=radio3]').addClass('collapse-alert-warning');
+    } else {
+        $('#collapseExample3').collapse('hide');
+        $('div[data-quantity=radio3]').removeClass('collapse-alert-warning');
+    }
+    if ($("#radio4").is(':checked')) {
+        $('#collapseExample4').collapse('show');
+        $('div[data-quantity=radio4]').addClass('collapse-alert-warning');
+    } else {
+        $('#collapseExample4').collapse('hide');
+        $('div[data-quantity=radio4]').removeClass('collapse-alert-warning');
+    }
+    if ($("#radio5").is(':checked')) {
+        $('#collapseExample5').collapse('show');
+        $('div[data-quantity=radio5]').addClass('collapse-alert-warning');
+    } else {
+        $('#collapseExample5').collapse('hide');
+        $('div[data-quantity=radio5]').removeClass('collapse-alert-warning');
+    }
+});
+
+$('input[name=payment]').on('change', function() {
+    if ($("#p1").is(':checked')) {
+        $('div[data-quantity=p1]').addClass('collapse-alert-warning');
+    } else {
+        $('div[data-quantity=p1]').removeClass('collapse-alert-warning');
+    }
+    if ($("#p2").is(':checked')) {
+
+        $('div[data-quantity=p2]').addClass('collapse-alert-warning');
+    } else {
+        $('div[data-quantity=p2]').removeClass('collapse-alert-warning');
+    }
+    if ($("#p3").is(':checked')) {
+        $('#p3').collapse('show');
+        $('div[data-quantity=p3]').addClass('collapse-alert-warning');
+    } else {
+        $('#collapseExample3').collapse('hide');
+        $('div[data-quantity=p3]').removeClass('collapse-alert-warning');
+    }
 
 });
 
- 
- $('input[name=radio]').on('change', function(){  
-  if($("#radio1").is(':checked')){
-    $('#collapseExample1').collapse('show');
-    $('div[data-quantity=radio1]').addClass('collapse-alert-warning');
-  }
-  else {
-    $('#collapseExample1').collapse('hide');
-    $('div[data-quantity=radio1]').removeClass('collapse-alert-warning');
-  }
-  if($("#radio2").is(':checked')){
-    $('#collapseExample2').collapse('show');
-    $('div[data-quantity=radio2]').addClass('collapse-alert-warning');
-
-  }
-  else {
-    $('#collapseExample2').collapse('hide');
-    $('div[data-quantity=radio2]').removeClass('collapse-alert-warning');
-  }
-  if($("#radio3").is(':checked')){
-    $('#collapseExample3').collapse('show');
-    $('div[data-quantity=radio3]').addClass('collapse-alert-warning');
-  }
-  else {
-    $('#collapseExample3').collapse('hide');
-    $('div[data-quantity=radio3]').removeClass('collapse-alert-warning');
-  }
-  if($("#radio4").is(':checked')){
-    $('#collapseExample4').collapse('show');
-    $('div[data-quantity=radio4]').addClass('collapse-alert-warning');
-  }
-  else {
-    $('#collapseExample4').collapse('hide');
-    $('div[data-quantity=radio4]').removeClass('collapse-alert-warning');
-  }
-  if($("#radio5").is(':checked')){
-    $('#collapseExample5').collapse('show');
-    $('div[data-quantity=radio5]').addClass('collapse-alert-warning');
-  }
-  else {
-    $('#collapseExample5').collapse('hide');
-    $('div[data-quantity=radio5]').removeClass('collapse-alert-warning');
-  }
-});
-
-
-
- $('input[name=payment]').on('change', function(){  
-  if($("#p1").is(':checked')){
-    $('div[data-quantity=p1]').addClass('collapse-alert-warning');
-  }
-  else {
-    $('div[data-quantity=p1]').removeClass('collapse-alert-warning');
-  }
-  if($("#p2").is(':checked')){
-
-    $('div[data-quantity=p2]').addClass('collapse-alert-warning');
-  }
-  else {
-    $('div[data-quantity=p2]').removeClass('collapse-alert-warning');
-  }
-  if($("#p3").is(':checked')){
-    $('#p3').collapse('show');
-    $('div[data-quantity=p3]').addClass('collapse-alert-warning');
-  }
-  else {
-    $('#collapseExample3').collapse('hide');
-    $('div[data-quantity=p3]').removeClass('collapse-alert-warning');
-  }
-
-
-});
-
-
- var findMeButton = $('.find-me');
+var findMeButton = $('.find-me');
 
 // Check if the browser has support for the Geolocation API
 if (!navigator.geolocation) {
 
-  findMeButton.addClass("disabled");
-  $('.no-browser-support').addClass("visible");
+    findMeButton.addClass("disabled");
+    $('.no-browser-support').addClass("visible");
 
 } else {
 
-  findMeButton.on('click', function(e) {
+    findMeButton.on('click', function(e) {
 
-    e.preventDefault();
+        e.preventDefault();
 
-    navigator.geolocation.getCurrentPosition(function(position) {
+        navigator.geolocation.getCurrentPosition(function(position) {
 
-      // Get the coordinates of the current possition.
-      var lat = position.coords.latitude;
-      var lng = position.coords.longitude;
+            // Get the coordinates of the current possition.
+            var lat = position.coords.latitude;
+            var lng = position.coords.longitude;
 
-      $('.latitude').text(lat.toFixed(3));
-      $('.longitude').text(lng.toFixed(3));
-      $('.coordinates').addClass('visible');
+            $('.latitude').text(lat.toFixed(3));
+            $('.longitude').text(lng.toFixed(3));
+            $('.coordinates').addClass('visible');
 
-      // Create a new map and place a marker at the device location.
+            // Create a new map and place a marker at the device location.
+
+        });
 
     });
 
-  });
-
 }
 
-
-
-
-$('#stepper1').on('click', function(){  
-  if($("#user-infomation").hasClass('show')){
-    $('#user-infomation').collapse('hide');
-    $('#user-infomation-succs').collapse('show');
-    $('#user-infomation-title').addClass('step-success');
-    $('#getting-infomation-title').removeClass('step-success');
-    $('#getting-infomation-succs').collapse('hide');
-  }
+$('#stepper1').on('click', function() {
+    if ($("#user-infomation").hasClass('show')) {
+        $('#user-infomation').collapse('hide');
+        $('#user-infomation-succs').collapse('show');
+        $('#user-infomation-title').addClass('step-success');
+        $('#getting-infomation-title').removeClass('step-success');
+        $('#getting-infomation-succs').collapse('hide');
+    }
 });
-$('#stepper2').on('click', function(){  
-  if($("#getting-information").hasClass('show')){
-    $('#getting-information').collapse('hide');
-    $('#getting-infomation-succs').collapse('show');
-    $('#getting-infomation-title').addClass('step-success');
+$('#stepper2').on('click', function() {
+    if ($("#getting-information").hasClass('show')) {
+        $('#getting-information').collapse('hide');
+        $('#getting-infomation-succs').collapse('show');
+        $('#getting-infomation-title').addClass('step-success');
 
-  }
+    }
 });
 
-
-
-$('#stepper-back2').on('click', function(){  
-  if($('#payment-information').hasClass('show')){
-    $('#payment-information').collapse('hide');
-    $('#getting-information').collapse('show');
-    $('#getting-infomation-succs').collapse('hide');
-    $('#getting-infomation-title').removeClass('step-success');
-  }
+$('#stepper-back2').on('click', function() {
+    if ($('#payment-information').hasClass('show')) {
+        $('#payment-information').collapse('hide');
+        $('#getting-information').collapse('show');
+        $('#getting-infomation-succs').collapse('hide');
+        $('#getting-infomation-title').removeClass('step-success');
+    }
 });
-$('#stepper-back1').on('click', function(){  
-  if($('#getting-information').hasClass('show')||$('#payment-information').hasClass('show')){
-    $('#getting-information').collapse('hide');
-    $('#payment-information').collapse('hide');
-    $('#user-infomation').collapse('show');
-    $('#user-infomation-succs').collapse('hide');
-    $('#user-infomation-title').removeClass('step-success');
-  }
+$('#stepper-back1').on('click', function() {
+    if ($('#getting-information').hasClass('show') || $('#payment-information').hasClass('show')) {
+        $('#getting-information').collapse('hide');
+        $('#payment-information').collapse('hide');
+        $('#user-infomation').collapse('show');
+        $('#user-infomation-succs').collapse('hide');
+        $('#user-infomation-title').removeClass('step-success');
+    }
 });
 
-
-$(window).on('load', function(){
-      var win = $(this); //this = window
-      if (win.width() >= 768) { 
+$(window).on('load', function() {
+    var win = $(this);
+    //this = window
+    if (win.width() >= 768) {
         $('#collapseExample-smart').addClass('show');
         $('#collapseExample-smart').removeClass('product-collapse');
         $('#product-collapse-plate').addClass('hidden-xl-down');
-      }
-    });
-
-
-
-
-
-
-var width = $(window).width();
-$(window).on('resize', function(){
- if($(this).width() != width){
-  width = $(this).width();
-  if (width >= 768) { 
-    $('#collapseExample-smart').addClass('show');
-    $('#collapseExample-smart').removeClass('product-collapse');
-    $('#product-collapse-plate').addClass('hidden-xl-down');
-  }
-  else{
-    $('#collapseExample-smart').removeClass('show');
-    $('#collapseExample-smart').addClass('product-collapse');
-    $('#product-collapse-plate').removeClass('hidden-xl-down');
-  }
-  
-}
+    }
 });
 
-
-var kolich = new Vue({
-  el: '#count',
-  data: {
-    prise: 12599,
-    stringprise: '12 599',
-    summ: 12599,
-    stringsumm: '12 599',
-    count: 1,
-    seen: false,
-    serviseListItem:[],
-    serviseListItemSum:'',
-
-  },
-  methods: {
-    downcount: function () {
-      if (this.count != 1) {
-        this.count = this.count-1;
-        this.summ = this.prise*this.count;
-        this.stringsumm=this.summ.toLocaleString();
-        if (this.count == 1) {
-          this.seen = false;
+var width = $(window).width();
+$(window).on('resize', function() {
+    if ($(this).width() != width) {
+        width = $(this).width();
+        if (width >= 768) {
+            $('#collapseExample-smart').addClass('show');
+            $('#collapseExample-smart').removeClass('product-collapse');
+            $('#product-collapse-plate').addClass('hidden-xl-down');
+        } else {
+            $('#collapseExample-smart').removeClass('show');
+            $('#collapseExample-smart').addClass('product-collapse');
+            $('#product-collapse-plate').removeClass('hidden-xl-down');
         }
 
-        for(var i=0;i<this.serviseListItem.length;i++){
-          this.serviseListItemSum = this.serviseListItemSum + this.serviseListItem[i];
+    }
+});
 
-        };
+var kolich = new Vue({
+    el: '#count',
+    data: {
+        prise: 12599,
+        stringprise: '12 599',
+        summ: 12599,
+        stringsumm: '12 599',
+        count: 1,
+        seen: false,
+        serviseListItem: [],
+        serviseListItemSum: '',
 
-        
-      }
-      else{
-        this.seen = false;
-      }
     },
-    upcount: function () {
-      this.count = this.count+1;
-      this.summ = this.prise*this.count;
-      this.seen = true;
-      this.stringsumm=this.summ.toLocaleString()
-    },
-    countinput: function () {
-      this.summ = this.prise*this.count;
-      this.stringsumm=this.summ.toLocaleString()
+    methods: {
+        serviseApp:function() {
+          this.serviseListItemSum = this.serviseListItem.reduce(function(sum, current) {
+                return sum + current;
+                }, 0);
+
+        },
+        downcount: function() {
+            if (this.count != 1) {
+                this.count = this.count - 1;
+                this.summ = this.prise * this.count;
+                this.stringsumm = this.summ.toLocaleString();
+                if (this.count == 1) {
+                    this.seen = false;
+                };
+
+                
+            } else {
+                this.seen = false;
+            }
+        },
+        upcount: function() {
+            this.count = this.count + 1;
+            this.summ = this.prise * this.count;
+            this.seen = true;
+            this.stringsumm = this.summ.toLocaleString()
+        },
+        countinput: function() {
+            this.summ = this.prise * this.count;
+            this.stringsumm = this.summ.toLocaleString()
+        }
+
     }
 
-  }
-
 })
+
+
+
