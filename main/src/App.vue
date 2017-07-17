5ea8fdd997eb01039d1163a4b0078331ff540472
logunoveugene <logunov.design@yandex.ru>
2017-07-17 22:52:20 +1000

    1

---
+++ main/src/App.vue
@@ -0,0 +1,63 @@
+Vue.use(VueAwesomeSwiper);
+
+
+
+<template>
+<div id="app">
+
+<div class="container">
+  <div class="row">
+    <div class="col-12 mt-4 mb-4">
+      <nav class=" small pt-3">
+        <a class="breadcrumb-item" href="#">Главная</a>
+        <a class="breadcrumb-item" href="#">Телевизоры и видеотехника</a>
+        <a class="breadcrumb-item" href="#">Телевизоры </a>
+      </nav>
+    </div>
+  </div>
+  <div class="row mb-4">
+    <div class="col shrink">
+      <h1 class="h3">Телевизор SAMSUNG UE19H4000AK </h1>
+    </div>
+    <div class="col d-flex flex-row-reverse align-items-center align-self-end pr-0  pt-2 pb-2">
+      <div class="small text-muted">арт. 12004252</div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-5 col-12">
+<slider></slider>
+
+    </div>
+    <div class="col-md-7">
+      <div class="row">
+        <div class="col-6">154</div>
+        <div class="col-6">599</div>
+      </div>
+    </div>
+  </div>
+</div>
+
+</div>
+
+</template>
+
+<script>
+
+
+import  slider from './slider.vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
+
+
+
+export default {
+  name: 'app',
+  components: {
+    slider, VueAwesomeSwiper
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ main/src/slider.vue
@@ -0,0 +1,122 @@
+<!-- The ref attr used to find the swiper instance -->
+Vue.use(VueTabs);
+
+<template>
+<div>
+  <swiper :options="swiperOptionTop" class="gallery-top" ref="swiperTop">
+    <!-- slides -->
+        <swiper-slide v-for="slide in swiperSlides" class="swiper-zoom-container">
+          <div class="image-warp d-flex align-items-center justify-content-center">
+            <img :data-src="slide" class="swiper-lazy large-image">
+            <div class="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
+          </div>
+        </swiper-slide>
+  </swiper>
+
+<swiper :options="swiperOptionThumbs" class="gallery-thumbs" ref="swiperThumbs">
+        <swiper-slide v-for="slide in swiperSlidesThumbs" class="d-flex align-items-center justify-content-center">
+        <div class="">
+          <img :src="slide" >
+         </div>
+        </swiper-slide>
+        </swiper>  
+</div>
+
+</template>
+
+<script>
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
+
+  // swiper options example:
+  export default {
+    name: 'slider',
+    components: {
+    swiper,
+    swiperSlide
+  },
+    data() {
+      return {
+        swiperOptionTop: {
+          // Disable preloading of all images
+          preloadImages: true,
+          // Enable lazy loading
+          lazyLoading: true,
+          notNextTick: true,
+          zoom: true
+        },
+        swiperOptionThumbs: {
+          notNextTick: true,
+          spaceBetween: 10,
+          centeredSlides: true,
+          slidesPerView: 'auto',
+          touchRatio: 0.2,
+          slideToClickedSlide: true
+
+        },
+      swiperSlides: [
+          "https://cdn.domotekhnika.ru/images/App/Models/Catalog/ProductImage/images/002/711/530/safe_original/12004913_1.jpg",
+          "https://cdn.domotekhnika.ru/images/App/Models/Catalog/ProductImage/images/002/713/024/safe_original/12004913_2.jpg",
+          "https://cdn.domotekhnika.ru/images/App/Models/Catalog/ProductImage/images/002/713/025/safe_original/12004913_3.jpg",
+          "https://cdn.domotekhnika.ru/images/App/Models/Catalog/ProductImage/images/002/713/026/safe_original/12004913_4.jpg",
+          "https://cdn.domotekhnika.ru/images/App/Models/Catalog/ProductImage/images/002/713/027/safe_original/12004913_5.jpg", 
+          "https://cdn.domotekhnika.ru/images/App/Models/Catalog/ProductImage/images/002/713/028/safe_original/12004913_6.jpg" 
+          ],
+        swiperSlidesThumbs: [
+          "https://cdn.domotekhnika.ru/images/App/Models/Catalog/ProductImage/images/002/711/530/thumb/12004913_1.jpg",
+          "https://cdn.domotekhnika.ru/images/App/Models/Catalog/ProductImage/images/002/713/024/thumb/12004913_2.jpg",
+          "https://cdn.domotekhnika.ru/images/App/Models/Catalog/ProductImage/images/002/713/025/thumb/12004913_3.jpg",
+          "https://cdn.domotekhnika.ru/images/App/Models/Catalog/ProductImage/images/002/713/026/thumb/12004913_4.jpg",
+          "https://cdn.domotekhnika.ru/images/App/Models/Catalog/ProductImage/images/002/713/027/thumb/12004913_5.jpg", 
+          "https://cdn.domotekhnika.ru/images/App/Models/Catalog/ProductImage/images/002/713/028/thumb/12004913_6.jpg" 
+          ]
+
+      }
+    },
+    // you can find current swiper instance object like this, while the notNextTick property value must be true
+    mounted() {
+      const swiperTop = this.$refs.swiperTop.swiper
+      const swiperThumbs = this.$refs.swiperThumbs.swiper
+      swiperTop.params.control = swiperThumbs
+      swiperThumbs.params.control = swiperTop
+    }
+ 
+  }
+</script>
+
+<style scoped>
+  .swiper-container {
+
+  }
+  .swiper-slide {
+    background-size: cover;
+    background-position: center;
+  }
+  .gallery-top {
+    height: 80%!important;
+    width: 100%;
+  }
+  .gallery-thumbs {
+    height: 20%!important;
+    box-sizing: border-box;
+    padding: 10px 0;
+  }
+  .gallery-thumbs .swiper-slide {
+    width: 50px;
+  height: 50px;
+    opacity: 0.4;
+    padding:.5rem;
+  }
+  .gallery-thumbs .swiper-slide-active {
+    opacity: 1;
+    border:1px solid #eee;
+  }
+.large-image{
+  max-height: 320px;
+  max-width: 100%;
+}
+.image-warp{
+  height: 320px;
+}
+
+</style>
