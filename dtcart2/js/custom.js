5771b7f714e6d706ba73f09a812396e18e6943cc
logunoveugene <logunov.design@yandex.ru>
2017-08-15 12:09:37 +1000

    1

--- dtcart2/js/custom.js
+++ dtcart2/js/custom.js
@@ -201,6 +201,7 @@ $(window).on('resize', function() {
 var kolich = new Vue({
     el: '#count',
     data: {
+        companyDetal: false,
         prise: 12599,
         stringprise: '12 599',
         summ: 12599,
@@ -214,196 +215,171 @@ var kolich = new Vue({
         serviseListItemCOUNT:0,
         orderSum: 12599,
         companyDetal: false,
-
-    },
-    methods: {
-        serviseApp:function() {
-          this.serviseListItemCOUNT = this.serviseListItem.length * this.count;
-
-          this.serviseListItemSum = this.serviseListItem.reduce(function(sum, current) {
-            return sum + current;
-        }, 0);
-          this.serviseListItemSumCOUNT = this.serviseListItemSum * this.count;
-
-          if (this.serviseListItem != 0) {
-            this.seenService = true;
-        }
-        else{
-            this.seenService = false;
-        };
-
-        this.orderSum = this.summ + this.serviseListItemSumCOUNT;
-
-    },
-    downcount: function() {
-        if (this.count != 1) {
-            this.count = this.count - 1;
-            this.summ = this.prise * this.count;
-            this.stringsumm = this.summ.toLocaleString();
-            if (this.count == 1) {
-                this.seen = false;
-            };
-
-
-        } else {
-            this.seen = false;
-        };
-        this.serviseListItemSumCOUNT = this.serviseListItemSum * this.count;
-        this.serviseListItemCOUNT = this.serviseListItem.length * this.count;
-        this.orderSum = this.summ + this.serviseListItemSumCOUNT;
-    },
-    upcount: function() {
-        this.count = this.count + 1;
-        this.summ = this.prise * this.count;
-        this.seen = true;
-        this.stringsumm = this.summ.toLocaleString();
-        this.serviseListItemSumCOUNT = this.serviseListItemSum * this.count;
-        this.serviseListItemCOUNT = this.serviseListItem.length * this.count;
-        this.orderSum = this.summ + this.serviseListItemSumCOUNT;
-    },
-    countinput: function() {
-        this.summ = this.prise * this.count;
-        this.stringsumm = this.summ.toLocaleString();
-        this.serviseListItemSumCOUNT = this.serviseListItemSum * this.count;
-        this.serviseListItemCOUNT = this.serviseListItem.length * this.count;
-        this.orderSum = this.summ + this.serviseListItemSumCOUNT;
-    }
-
-}
-
-});
-
-
-
-
-var userDetal = new Vue({
-    el: '#userComp',
-    data: {
-
-        companyDetal: false,
-
-    },
-    methods: {
-        companydetal:function() {
-            if (this.companyDetal === "false") {
-                this.companyDetal = false;
-            }
-
-        },
-        step2:function() {
-
-            if ($("#user-infomation").hasClass('show')) {
-                $('#user-infomation').collapse('hide');
-                $('#user-infomation-succs').collapse('show');
-                $('#user-infomation-title').addClass('step-success');
-                $('#getting-infomation-title').removeClass('step-success');
-                $('#getting-infomation-succs').collapse('hide');
-            }
-
-
-        },
-        step1:function() {
-
-          if ($('#getting-information').hasClass('show') || $('#payment-information').hasClass('show')) {
-            $('#getting-information').collapse('hide');
-            $('#payment-information').collapse('hide');
-            $('#user-infomation').collapse('show');
-            $('#user-infomation-succs').collapse('hide');
-            $('#user-infomation-title').removeClass('step-success');
-        }
-
-    },
-
-
-}
-
-
-});
-
-
-
-
-var userDetal = new Vue({
-    el: '#chooseGift',
-    data: {
-                    private: {
-                gifts : [
-                {
-                    name : 'Батарея Flama FLB-LP-E10"',
-                    img: "https://cdn.domotekhnika.ru/images/App/Models/Catalog/ProductImage/images/002/526/872/catalog_tile/12500525_1.jpg",
-                    price: 600,
-         
-                    stores : [ {storename:"в ТЦ Черемушки"}, {storename:"на Гоголя"}, {storename:"на Алеутской"}] 
-                },
-
-                {
-                  name : 'Крышка для объектива Flama Ф82',
-                  img: "https://cdn.domotekhnika.ru/images/App/Models/Catalog/ProductImage/images/002/513/785/catalog_tile/31002347_1.jpg",
-                  price: 950,
-         
-                  stores : [ {storename:"в ТЦ Черемушки"}, {storename:"на Гоголя"}, {storename:"на Алеутской"}] 
-              },
-              {
-                name:"Пленка защитная Kenko",
-                img: "https://cdn.domotekhnika.ru/images/App/Models/Catalog/ProductImage/images/002/515/812/catalog_tile/31002388_1.jpg",
-                price: 620,
-       
-                stores : [ {storename:"в ТЦ Черемушки"}, {storename:"на Гоголя"}, {storename:"на Алеутской"}] 
+        private: {
+            gifts : [
+            {
+                name : 'Батарея Flama FLB-LP-E10"',
+                img: "https://cdn.domotekhnika.ru/images/App/Models/Catalog/ProductImage/images/002/526/872/catalog_tile/12500525_1.jpg",
+                price: 600,
+                
+                stores : [ {storename:"в ТЦ Черемушки"}, {storename:"на Алеутской"}] 
             },
 
             {
-              name : 'Вспышка Nissin Di466N ',
-              img: "https://cdn.domotekhnika.ru/images/App/Models/Catalog/ProductImage/images/002/486/385/catalog_tile/31002090_1.jpg",
-            price: 800,
-
-              stores : [ {storename:"в ТЦ Черемушки"}, {storename:"на Гоголя"}, {storename:"на Алеутской"}] 
+              name : 'Крышка для объектива Flama Ф82',
+              img: "https://cdn.domotekhnika.ru/images/App/Models/Catalog/ProductImage/images/002/513/785/catalog_tile/31002347_1.jpg",
+              price: 950,
+              
+              stores : [ {storename:"на Гоголя"}, {storename:"на Алеутской"}] 
           },
           {
-              name : 'Штатив Sony VCT-R100 ',
-               img: "https://cdn.domotekhnika.ru/images/App/Models/Catalog/ProductImage/images/002/513/681/catalog_tile/31000996_1.jpg",
-            price: 990,
-    
-              stores : [ {storename:"в ТЦ Черемушки"}, {storename:"на Гоголя"}, {storename:"на Алеутской"}] 
-          }
+            name:"Пленка защитная Kenko",
+            img: "https://cdn.domotekhnika.ru/images/App/Models/Catalog/ProductImage/images/002/515/812/catalog_tile/31002388_1.jpg",
+            price: 620,
+            
+            stores : [ {storename:"в ТЦ Черемушки"}, {storename:"на Гоголя"}, {storename:"на Алеутской"}] 
+        },
+
+        {
+          name : 'Вспышка Nissin Di466N ',
+          img: "https://cdn.domotekhnika.ru/images/App/Models/Catalog/ProductImage/images/002/486/385/catalog_tile/31002090_1.jpg",
+          price: 800,
+
+          stores : [ {storename:"в ТЦ Черемушки"},  {storename:"на Алеутской"}] 
+      },
+      {
+          name : 'Штатив Sony VCT-R100 ',
+          img: "https://cdn.domotekhnika.ru/images/App/Models/Catalog/ProductImage/images/002/513/681/catalog_tile/31000996_1.jpg",
+          price: 990,
+          
+          stores : [ {storename:"в ТЦ Черемушки"}, {storename:"на Гоголя"}, ] 
+      }
 
 
-          ]
-    },selectedPrivate: {
-        selectedGifts : []},
-        filter: "",    
-        giftcountset:2,
-        giftsum:3000,
-        selectedGiftcount:0,
-        selectedGiftsum:0
+      ]
+  },selectedPrivate: {
+    selectedGifts : []},
+    filter: "",    
+    giftcountset:2,
+    giftsum:3000,
+    selectedGiftcount:0,
+    selectedGiftsum:0
+
+},
+
+
+methods: {
+    companydetal:function() {
+        if (this.companyDetal === "false") {
+            this.companyDetal = false;
+        }
 
     },
-    
+    step2:function() {
 
-    methods: {
-    addNew: function (index) {
-        if (this.selectedPrivate.selectedGifts.length < this.giftcountset) {
+        if ($("#user-infomation").hasClass('show')) {
+            $('#user-infomation').collapse('hide');
+            $('#user-infomation-succs').collapse('show');
+            $('#user-infomation-title').addClass('step-success');
+            $('#getting-infomation-title').removeClass('step-success');
+            $('#getting-infomation-succs').collapse('hide');
+        }
+
+
+    },
+    step1:function() {
+
+      if ($('#getting-information').hasClass('show') || $('#payment-information').hasClass('show')) {
+        $('#getting-information').collapse('hide');
+        $('#payment-information').collapse('hide');
+        $('#user-infomation').collapse('show');
+        $('#user-infomation-succs').collapse('hide');
+        $('#user-infomation-title').removeClass('step-success');
+    }
+
+},
+addNew: function (index) {
+    if (this.selectedPrivate.selectedGifts.length < this.giftcountset) {
       this.selectedPrivate.selectedGifts.push(this.filteredgifts[index])}
       
-    },
-    del: function(index) {
-        this.selectedPrivate.selectedGifts.splice(index,1);
-    
-    }
   },
-  computed: {
+  del: function(index) {
+    this.selectedPrivate.selectedGifts.splice(index,1);
+    
+},
+serviseApp:function() {
+  this.serviseListItemCOUNT = this.serviseListItem.length * this.count;
+
+  this.serviseListItemSum = this.serviseListItem.reduce(function(sum, current) {
+    return sum + current;
+}, 0);
+  this.serviseListItemSumCOUNT = this.serviseListItemSum * this.count;
+
+  if (this.serviseListItem != 0) {
+    this.seenService = true;
+}
+else{
+    this.seenService = false;
+};
+
+this.orderSum = this.summ + this.serviseListItemSumCOUNT;
+
+},
+downcount: function() {
+    if (this.count != 1) {
+        this.count = this.count - 1;
+        this.summ = this.prise * this.count;
+        this.stringsumm = this.summ.toLocaleString();
+        if (this.count == 1) {
+            this.seen = false;
+        };
+
+
+    } else {
+        this.seen = false;
+    };
+    this.serviseListItemSumCOUNT = this.serviseListItemSum * this.count;
+    this.serviseListItemCOUNT = this.serviseListItem.length * this.count;
+    this.orderSum = this.summ + this.serviseListItemSumCOUNT;
+},
+upcount: function() {
+    this.count = this.count + 1;
+    this.summ = this.prise * this.count;
+    this.seen = true;
+    this.stringsumm = this.summ.toLocaleString();
+    this.serviseListItemSumCOUNT = this.serviseListItemSum * this.count;
+    this.serviseListItemCOUNT = this.serviseListItem.length * this.count;
+    this.orderSum = this.summ + this.serviseListItemSumCOUNT;
+},
+countinput: function() {
+    this.summ = this.prise * this.count;
+    this.stringsumm = this.summ.toLocaleString();
+    this.serviseListItemSumCOUNT = this.serviseListItemSum * this.count;
+    this.serviseListItemCOUNT = this.serviseListItem.length * this.count;
+    this.orderSum = this.summ + this.serviseListItemSumCOUNT;
+}
+
+},
+computed: {
   filteredgifts: function () {
     var self = this
     return self.private.gifts.filter(function (folder) {
       return folder.name.indexOf(self.filter) !== -1
-    })
-  },
-  selectedGiftcountq:function () {
+  })
+},
+selectedGiftcountq:function () {
   for(var i = 0, len = this.selectedPrivate.selectedGifts.length; i < len; i++) {
-    this.selectedGiftsum += this.selectedPrivate.selectedGifts.price[i][1];  }
+    this.selectedGiftsum += this.selectedPrivate.selectedGifts.price[i];  }
 }
 }
 
 });
+
+
+
+
+
+
 
 
 
