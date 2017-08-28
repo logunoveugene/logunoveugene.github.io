c3acceae26fc65026ee13c33bef25e3a82e66271
logunoveugene <logunov.design@yandex.ru>
2017-08-28 15:15:27 +1000

    1

--- catalog/js/custom.js
+++ catalog/js/custom.js
@@ -21,6 +21,22 @@ $(document).ready(function () {
 
   });
 
+  
+
+var handlesSlider = document.getElementById('slider-handles');
+
+noUiSlider.create(handlesSlider, {
+  start: [ 4000, 8000 ],
+  range: {
+    'min': [  2000 ],
+    'max': [ 10000 ]
+  }
+});
+
+
+
+
+
   });
 
   $(document).on('click', function(event) {
