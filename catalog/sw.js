7868a4892079c1178406f60ecbcb008bfb959b94
logunoveugene <logunov.design@yandex.ru>
2018-02-01 11:00:24 +1000

    1

--- catalog/sw.js
+++ catalog/sw.js
@@ -2,8 +2,24 @@ const
   cacheName = 'v2',
   cacheFiles = [
     './',
-    './index.css',
-    './tether.min.js',
+      './css/styles.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.min.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.2/css/swiper.min.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/noUiSlider/11.0.0/nouislider.min.css',
+    './css/c-style.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/wnumb/1.1.0/wNumb.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/js/bootstrap.min.js',
+    'https://unpkg.com/vue',
+    'https://cdnjs.cloudflare.com/ajax/libs/hammer.js/2.0.8/hammer.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.2/js/swiper.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/noUiSlider/11.0.0/nouislider.min.js',
+    './js/sticky-sidebar.min.js',
+    './js/tether.min.js',
+    './js/vue-touch.min.js',
+    './js/rate.vue',
+    './js/custom.js'
+
 
   ];
 
