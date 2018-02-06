c98cd15876b8d561d4afd020cb5483fe8070e84a
logunoveugene <logunov.design@yandex.ru>
2018-02-06 11:41:40 +1000

    1

--- catalog/js/index.js
+++ catalog/js/index.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
 
   navigator.serviceWorker
-    .register('./sw.js', { scope: './index.html' })
+    .register('./sw.js', { scope: './' })
     .then(function(registration) {
       console.log("Service Worker Registered");
     })
