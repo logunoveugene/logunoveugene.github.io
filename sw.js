d6050be838725ae0228032d62def15f8fca8062a
logunoveugene <logunov.design@yandex.ru>
2018-02-06 12:06:06 +1000

    1

--- sw.js
+++ sw.js
@@ -5,17 +5,17 @@ var cacheName = 'v1';
 // Default files to always cache
 var cacheFiles = [
 './',
-   'index.html',
+  'index.html',
   './index.html',
-    'css/styles.css',
+    './css/styles.css',
     'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.min.css',
     'https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.2/css/swiper.min.css',
     'https://cdnjs.cloudflare.com/ajax/libs/noUiSlider/11.0.0/nouislider.min.css',
-    'css/c-style.css',
-    'css/fonts/icons.woff',
-    'css/fonts/icons.eot',
-    'css/fonts/icons.svg',
-    'css/fonts/icons.ttf',
+    './css/c-style.css',
+    './css/fonts/icons.woff',
+    './css/fonts/icons.eot',
+    './css/fonts/icons.svg',
+    './css/fonts/icons.ttf',
     'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/wnumb/1.1.0/wNumb.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/js/bootstrap.min.js',
@@ -23,11 +23,11 @@ var cacheFiles = [
     'https://cdnjs.cloudflare.com/ajax/libs/hammer.js/2.0.8/hammer.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.2/js/swiper.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/noUiSlider/11.0.0/nouislider.min.js',
-    'js/sticky-sidebar.min.js',
-    'js/tether.min.js',
-    'js/vue-touch.min.js',
-    'js/rate.vue',
-    'js/custom.js'
+    './js/sticky-sidebar.min.js',
+    './js/tether.min.js',
+    './js/vue-touch.min.js',
+    './js/rate.vue',
+    './js/custom.js'
 ]
 
 
