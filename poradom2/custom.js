d14c36a9f8dd6c6bc6a36c25c4a1a6795eb77424
logunoveugene <logunov.design@yandex.ru>
2018-02-20 11:48:12 +1000

    1

---
+++ poradom2/custom.js
@@ -0,0 +1,37 @@
+var swiper = new Swiper('.swiper-main', {
+      speed: 600,
+      parallax: true,
+slidesPerView: 'auto',
+      centeredSlides: true,
+autoHeight: true, 
+autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+    });
+
+
+
+var swiper = new Swiper('.swiper-poselok', {
+      slidesPerView: 'auto',
+      spaceBetween: 15,
+      freeMode: true,
+
+    });
+
+var swiper = new Swiper('.swiper-project-menu', {
+      slidesPerView: 'auto',
+      freeMode: true,
+
+    });
+var swiper = new Swiper('.swiper-project', {
+      slidesPerView: 'auto',
+      freeMode: true,
+
+    });
+
+
+
+
+
+
