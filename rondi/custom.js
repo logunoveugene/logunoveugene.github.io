ff0e80e449650adedee1dbf8eb540c279d6e5844
logunoveugene <logunov.design@yandex.ru>
2018-02-22 17:12:41 +1000

    1

--- rondi/custom.js
+++ rondi/custom.js
@@ -1,18 +1,18 @@
 var swiper = new Swiper('.swiper-main', {
   speed: 600,
-   spaceBetween: 30,
+  spaceBetween: 30,
   parallax: true,
   slidesPerView: 'auto',
   centeredSlides: true,
- loop: true,
-        lazy: {
+  loop: true,
+  lazy: {
     loadPrevNext: true,
     loadPrevNextAmount:3,
   },
-   pagination: {
-        el: '.swiper-pagination',
-        dynamicBullets: true,
-      }
+  pagination: {
+    el: '.swiper-pagination',
+    dynamicBullets: true,
+  }
 
 });
 
@@ -21,3 +21,10 @@ var swiper = new Swiper('.swiper-main', {
 
 
 
+$('div[data-act=modal]').click(function(e) {
+  $('#work').modal();
+  var soure = $(this).attr('data-src');
+  console.log(soure);
+$("#result").load("hi.html");
+
+});
