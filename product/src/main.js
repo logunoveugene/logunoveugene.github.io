7f9dcad58d44826d4d527ec19e1768a02993f884
logunoveugene <logunov.design@yandex.ru>
2018-05-04 12:42:23 +1000

    1

---
+++ product/src/main.js
@@ -0,0 +1,165 @@
+import Vue from 'vue'
+import App from './App.vue'
+import swiper from 'swiper';
+import 'sticky-sidebar';
+import 'tether';
+import './bootstrap/bootstrap';
+import './bootstrap/bootstrap.scss';
+
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
+
+
+
+var barW = 20; // 浮动条的宽度，px
+var btnW = 70; // 导航按钮的宽度，px
+var slideAmount = 9; // 分页数量
+
+$(document).ready(function () {
+
+
+var mainTabdSwiper = new Swiper('.mainTabdSwiper', {
+    preventClicks :true,
+    preventClicksPropagation: true,
+    slidesPerView: 'auto',
+    freeMode: true,
+    slidesOffsetAfter: 1
+  });
+
+
+var productRelated = new Swiper('.product-related', {
+    preventClicks :true,
+    preventClicksPropagation: true,
+    slidesPerView: 'auto',
+    slidesOffsetAfter: 1
+  });
+
+var breadcrumbsSwiper = new Swiper('.breadcrumbsSwiper', {
+    preventClicks :true,
+    preventClicksPropagation: true,
+    slidesPerView: 'auto',
+    slidesOffsetAfter: 1
+
+  });
+
+
+
+  var tabNavSwiper = new Swiper('.img-tab-nav', {
+    slidesPerView: 'auto',
+    direction: 'vertical',
+    scrollbar: '.swiper-scrollbar',
+    scrollbarHide: false
+    
+  });
+
+  var tabContentSwiper = new Swiper('.img-tab-content', {
+  	preloadImages: false,
+direction: 'vertical',
+    lazyLoading: true,
+    keyboardControl: true,
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+
+    onSlideChangeStart: function onSlideChangeStart(swiper) {
+      $('#tabNav .active').removeClass('active');
+      $('#tabNav .swiper-slide[data-slide-index=' + swiper.activeIndex + ']').addClass('active');
+      if (swiper.previousIndex < swiper.activeIndex) {
+        tabNavSwiper.slideTo(swiper.activeIndex - 2);
+      } else {
+        tabNavSwiper.slideTo(swiper.activeIndex - 1);
+      }
+    }
+  });
+
+  $('#tabNav .swiper-slide').on('click', function (event) {
+    tabContentSwiper.slideTo($(this).data('slide-index'));
+  });
+
+
+
+
+var fulltabNavSwiper = new Swiper('.full-tab-nav', {
+    slidesPerView: 'auto',
+    scrollbarHide: false,
+      observer: true,
+  observeParents: true,
+    
+  });
+
+
+  var fulltabContentSwiper = new Swiper('.full-tab-content', {
+    preloadImages: false,
+    lazyLoading: true,
+    zoom: true,
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+      observer: true,
+  observeParents: true,
+
+    onSlideChangeStart: function onSlideChangeStart(swiper) {
+      $('#full-tabNav .active').removeClass('active');
+      $('#full-tabNav .swiper-slide[data-slide-index=' + swiper.activeIndex + ']').addClass('active');
+      if (swiper.previousIndex < swiper.activeIndex) {
+        fulltabNavSwiper.slideTo(swiper.activeIndex - 2);
+      } else {
+        fulltabNavSwiper.slideTo(swiper.activeIndex - 1);
+      }
+    }
+  });
+
+
+
+
+  $('#full-tabNav .swiper-slide').on('click', function (event) {
+    fulltabContentSwiper.slideTo($(this).data('slide-index'));
+  });
+
+
+
+
+$('#fullscreen-image').on('shown.bs.modal', function (event) {
+  setTimeout(function () {
+  fulltabContentSwiper.update;
+  fulltabNavSwiper.update;
+  }, 500);
+});
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip(
+  {
+        trigger: 'click',
+        constraints: [
+        {
+          to: 'scrollParent',
+          pin: true
+        },
+        ]})
+});
+
+ // var sidebar = new StickySidebar('#sidebar', {
+ //        containerSelector: '#main-content',
+ //        innerWrapperSelector: '.sidebar__inner',
+ //        topSpacing: 20,
+ //        bottomSpacing: 20
+
+ //    });
+
+$(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+    if (scroll > 600) {
+        $('#price').removeClass('hidden-xs-up'); 
+    };
+    if (scroll < 600) {
+        $('#price').addClass('hidden-xs-up'); 
+    };
+});
+
+
+
+});
+
+
+
