3dd3684599a2b29347727fe622583cb310c7d9a0
logunoveugene <logunov.design@yandex.ru>
2018-06-17 08:59:21 +1000

    1

--- club/js/custom.js
+++ club/js/custom.js
@@ -1,6 +1,20 @@
-var day = dayjs.extend(dayjs_plugin_relativeTime);
+
 
 $(document).ready(function () { 
+
+  var day = dayjs.extend(dayjs_plugin_relativeTime);
+
+
+  var config = {
+    apiKey: "AIzaSyAM5dSSOW6uZq-3e5oDyNWK-CkHAd66EX8",
+    authDomain: "club-74325.firebaseapp.com",
+    databaseURL: "https://club-74325.firebaseio.com",
+  }
+  firebase.initializeApp(config)
+
+  var usersRef = firebase.database().ref('posts')
+
+
   Vue.config.devtools = true;
   var main = new Vue({
     el: '#app',
@@ -11,9 +25,9 @@ $(document).ready(function () {
         type: 'alert-post',
         title: 'Изменения в правилах оформления блогов и другие важные аспекты для авторов',
         tags: [
-          'обзоры',
-          'смартфоны', 
-          'Vertex'
+        'обзоры',
+        'смартфоны', 
+        'Vertex'
         ],
         img: '',
         desc:'Эта запись создана для авторов, кто почему-то не следит за важными темами на форуме и по какой-то причине не получил письмо на почтовый ящик после обновления правил 07 марта 2018 года.',
@@ -26,9 +40,9 @@ $(document).ready(function () {
         type: 'base-post',
         title: 'Vertex Impress Tor: бюджетный защищенный смартфон с компактным «гражданским» дизайном и железом от Qualcomm!',
         tags: [
-          'обзоры',
-          'смартфоны', 
-          'Vertex'
+        'обзоры',
+        'смартфоны', 
+        'Vertex'
         ],
         img: 'https://i.snag.gy/NKnswr.jpg',
         desc:'Всем привет! Не так давно я написал обзор Vertex Impress Nero довольно интересного недорого смартфона с экраном 18:9 и металлическим корпусом. А сегодня речь пойдет о другом продукте того же бренда, а именно о защищенном смартфоне Vertex Impress Tor. Но для начала – немного лирики.',
@@ -41,9 +55,9 @@ $(document).ready(function () {
         type: 'base-post',
         title: 'Обзор принтера canon pixma ts5040',
         tags: [
-          'обзоры',
-          'принтеры',
-          'Canon'
+        'обзоры',
+        'принтеры',
+        'Canon'
         ],
         img: 'https://i.snag.gy/5utl3T.jpg',
         desc:'Прошли те времена, когда лазерный принтер могли позволить себе только организации. Лазерные принтеры до сих пор дороже струйных, но стоимость отпечатка (распечатанной страницы) у лазерных принтеров несравнимо ниже – настолько, что разница в цене может окупиться уже через тысячу-другую страниц. Качество текста на таких страницах будет лучше, чем у струйного, да и печатает лазерный принтер намного быстрее.',
@@ -61,8 +75,8 @@ $(document).ready(function () {
 
     filters: {
       fdate: function(value) {
-            return dayjs().from(dayjs(value));
-        }
+        return dayjs().from(dayjs(value));
+      }
     }
   });
 
