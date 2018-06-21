f14f78f7c25132cd58312128d81bc6478c05f4fc
eugene <logunovevg@Mac-mini-Pavel-2.local>
2018-06-21 19:03:37 +1000

    1

--- club/js/custom.js
+++ club/js/custom.js
@@ -1,88 +1,107 @@
 
 
 
+
+
 dayjs.locale('ru');
 
 
-  var day = dayjs.extend(dayjs_plugin_relativeTime);
+var day = dayjs.extend(dayjs_plugin_relativeTime);
 
 
-    var swiper = new Swiper('.swiper-asks', {
-      slidesPerView: 3,
-      spaceBetween: 30,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-    });
+var config = {
+  apiKey: "AIzaSyAM5dSSOW6uZq-3e5oDyNWK-CkHAd66EX8",
+  authDomain: "club-74325.firebaseapp.com",
+  databaseURL: "https://club-74325.firebaseio.com",
+}
 
+firebase.initializeApp(config);
 
+var postsRef = firebase.database().ref('posts');
+var askRef = firebase.database().ref('asks');
+var forumRef = firebase.database().ref('forum-items');
+var liveRef = firebase.database().ref('live');
+var usersRef = firebase.database().ref('top-users');
 
+Vue.config.devtools = true;
 
-  var config = {
-    apiKey: "AIzaSyAM5dSSOW6uZq-3e5oDyNWK-CkHAd66EX8",
-    authDomain: "club-74325.firebaseapp.com",
-    databaseURL: "https://club-74325.firebaseio.com",
+Vue.use(VueAwesomeSwiper)
+
+var main = new Vue({
+  el: '#app',
+  components: {
+    LocalSwiper: VueAwesomeSwiper.swiper,
+    LocalSlide: VueAwesomeSwiper.swiperSlide,
+  },
+
+  data: {
+    newAsk: {
+      product:"",
+      img: "",
+      desc:"",
+      author:"",
+      answer:0
+    },
+    swiperOptionB: {
+
+      spaceBetween:30,
+      slidesPerView: 'auto',
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      }
+
+    }
+  },
+
+  firebase: {
+    posts: postsRef,
+    asks: askRef,
+    forums: forumRef,
+    lives:liveRef,
+    topUsers:usersRef
+  },
+
+  methods: {
+    addAsk: function () {
+      this.newAsk.date = dayjs().format();
+      askRef.push(this.newAsk)
+      this.newAsk.product = ''
+      this.newAsk.img = ''
+      this.newAsk.desc = ''
+      this.newAsk.date = 
+      this.newAsk.author = ''
+      this.newAsk.answer = 0
+    },
+  },
+
+  filters: {
+    fdate: function(value) {
+      return dayjs().from(dayjs(value));
+    },
+    text180:function(value) {
+
+      var sliced = value.slice(0,180);
+      if (sliced.length < value.length) {
+        return sliced += '...';
+      }
+      return value;
+    },
+  },
+  computed: {
+    orderedAsks: function () {
+      return _.orderBy(this.asks, 'date', 'desc' )
+    },
+    orderedPosts: function () {
+      return _.orderBy(this.posts, 'date', 'desc' )
+    },
+    swiperB() {
+      return this.$refs.awesomeSwiperB.swiper
+    }
   }
-
-  firebase.initializeApp(config);
-
-  var postsRef = firebase.database().ref('posts');
-  var askRef = firebase.database().ref('asks');
-  var forumRef = firebase.database().ref('forum-items');
-  var liveRef = firebase.database().ref('live');
-  var usersRef = firebase.database().ref('top-users');
-
-  Vue.config.devtools = true;
-  var main = new Vue({
-    el: '#app',
-
-    data: {
-      newAsk: {
-        product:"",
-        img: "",
-        desc:"",
-          author:"",
-        answer:0
-    }
-    },
-
-    firebase: {
-      posts: postsRef,
-      asks: askRef,
-      forums: forumRef,
-      lives:liveRef,
-      topUsers:usersRef
-    },
-
-    methods: {
-      addAsk: function () {
-              this.newAsk.date = dayjs().format();
-              askRef.push(this.newAsk)
-              this.newAsk.product = ''
-              this.newAsk.img = ''
-              this.newAsk.desc = ''
-              this.newAsk.date = 
-              this.newAsk.author = ''
-              this.newAsk.answer = 0
-          },
-    },
-
-    filters: {
-      fdate: function(value) {
-        return dayjs().from(dayjs(value));
-      }
-    },
-    computed: {
-      orderedAsks: function () {
-        return _.orderBy(this.asks, 'date', 'desc' )
-      },
-      orderedPosts: function () {
-        return _.orderBy(this.posts, 'date', 'desc' )
-      }
-    }
-  });
+});
 
 
 
- 
+
+
