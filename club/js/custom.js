1b36ace34b29c32c2e9b11294694750677ee430e
eugene <logunovevg@Mac-mini-Pavel-2.local>
2018-06-26 18:13:47 +1000

    1

--- club/js/custom.js
+++ club/js/custom.js
@@ -1,10 +1,10 @@
   var observer = lozad('.lozad', {
-        threshold: 0.1,
-        load: function(el) {
-            el.src = el.getAttribute("data-src");
-       
-        }
-    })
+    threshold: 0.1,
+    load: function(el) {
+      el.src = el.getAttribute("data-src");
+
+    }
+  })
 
     // Picture observer
     // with default `load` method
@@ -17,119 +17,129 @@
 
 
 
-dayjs.locale('ru');
+    dayjs.locale('ru');
 
 
-var day = dayjs.extend(dayjs_plugin_relativeTime);
+    var day = dayjs.extend(dayjs_plugin_relativeTime);
 
 
-var config = {
-  apiKey: "AIzaSyAM5dSSOW6uZq-3e5oDyNWK-CkHAd66EX8",
-  authDomain: "club-74325.firebaseapp.com",
-  databaseURL: "https://club-74325.firebaseio.com",
-}
-
-firebase.initializeApp(config);
-
-var postsRef = firebase.database().ref('posts');
-var askRef = firebase.database().ref('asks');
-var forumRef = firebase.database().ref('forum-items');
-var liveRef = firebase.database().ref('live');
-var usersRef = firebase.database().ref('top-users');
-var newsRef = firebase.database().ref('news');
-var guidsRef = firebase.database().ref('guid');
-
-Vue.config.devtools = true;
-
-Vue.use(VueAwesomeSwiper)
-
-var main = new Vue({
-  el: '#app',
-  components: {
-    LocalSwiper: VueAwesomeSwiper.swiper,
-    LocalSlide: VueAwesomeSwiper.swiperSlide,
-  },
-
-  data: {
-    newAsk: {
-      product:"",
-      img: "",
-      desc:"",
-      author:"",
-      answer:0
-    },
-    live:"showlive",
-    
-    promoreg:true,
-
-    swiperOptionB: {
-
-      spaceBetween:30,
-      slidesPerView: 'auto',
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      }
-
+    var config = {
+      apiKey: "AIzaSyAM5dSSOW6uZq-3e5oDyNWK-CkHAd66EX8",
+      authDomain: "club-74325.firebaseapp.com",
+      databaseURL: "https://club-74325.firebaseio.com",
     }
-  },
 
-  firebase: {
-    posts: postsRef,
-    news: newsRef,
-    asks: askRef,
-    forums: forumRef,
-    lives: liveRef,
-    topUsers: usersRef,
-    guids: guidsRef
-    
-  },
+    firebase.initializeApp(config);
 
-  methods: {
-    addAsk: function () {
-      this.newAsk.date = dayjs().format();
-      askRef.push(this.newAsk)
-      this.newAsk.product = ''
-      this.newAsk.img = ''
-      this.newAsk.desc = ''
-      this.newAsk.date = 
-      this.newAsk.author = ''
-      this.newAsk.answer = 0
-    },
-    liveoff:function () {
-      this.live = "";
-    },
-    liveon:function () {
-      this.live = "showlive";
-    },
+    var postsRef = firebase.database().ref('posts');
+    var askRef = firebase.database().ref('asks');
+    var forumRef = firebase.database().ref('forum-items');
+    var liveRef = firebase.database().ref('live');
+    var usersRef = firebase.database().ref('top-users');
+    var newsRef = firebase.database().ref('news');
+    var guidsRef = firebase.database().ref('guid');
 
-  },
+    Vue.config.devtools = true;
 
-  filters: {
-    fdate: function(value) {
-      return dayjs().from(dayjs(value));
-    },
-    text180:function(value) {
+    Vue.use(VueAwesomeSwiper)
 
-      var sliced = value.slice(0,170);
-      if (sliced.length < value.length) {
-        return sliced += '...';
+    var main = new Vue({
+      el: '#app',
+      components: {
+        LocalSwiper: VueAwesomeSwiper.swiper,
+        LocalSlide: VueAwesomeSwiper.swiperSlide,
+      },
+
+      data: {
+        newAsk: {
+          product:"",
+          img: "",
+          desc:"",
+          author:"",
+          answer:0
+        },
+        live:"showlive",
+
+        promoreg:true,
+
+        swiperOptionB: {
+
+          spaceBetween:30,
+          slidesPerView: 'auto',
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          }
+
+        },
+        swiperOptionC: {
+          spaceBetween:30,
+          slidesPerView: 'auto',
+          mousewheel: true,
+          direction:"vertical",
+          scrollbar: {
+            el: '.swiper-scrollbar',
+            hide: true
+          }
+        }
+      },
+
+      firebase: {
+        posts: postsRef,
+        news: newsRef,
+        asks: askRef,
+        forums: forumRef,
+        lives: liveRef,
+        topUsers: usersRef,
+        guids: guidsRef
+
+      },
+
+      methods: {
+        addAsk: function () {
+          this.newAsk.date = dayjs().format();
+          askRef.push(this.newAsk)
+          this.newAsk.product = ''
+          this.newAsk.img = ''
+          this.newAsk.desc = ''
+          this.newAsk.date = 
+          this.newAsk.author = ''
+          this.newAsk.answer = 0
+        },
+        liveoff:function () {
+          this.live = "";
+        },
+        liveon:function () {
+          this.live = "showlive";
+        },
+
+      },
+
+      filters: {
+        fdate: function(value) {
+          return dayjs().from(dayjs(value));
+        },
+        text180:function(value) {
+
+          var sliced = value.slice(0,170);
+          if (sliced.length < value.length) {
+            return sliced += '...';
+          }
+          return value;
+        },
+      },
+      computed: {
+        orderedAsks: function () {
+          return _.orderBy(this.asks, 'date', 'desc' )
+        },
+        orderedPosts: function () {
+          return _.orderBy(this.posts, 'date', 'desc' )
+        },
+        swiperB() {
+          return this.$refs.awesomeSwiperB.swiper
+        }
       }
-      return value;
-    },
-  },
-  computed: {
-    orderedAsks: function () {
-      return _.orderBy(this.asks, 'date', 'desc' )
-    },
-    orderedPosts: function () {
-      return _.orderBy(this.posts, 'date', 'desc' )
-    },
-    swiperB() {
-      return this.$refs.awesomeSwiperB.swiper
-    }
-  }
-});
+    });
 
 
 
