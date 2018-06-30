ac5b0eed2e4e177ceabbde3fa3906f1c2497631f
logunoveugene <logunov.design@yandex.ru>
2018-06-30 14:35:42 +1000

    1

---
+++ evak/js/custom.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".swiper-container", {
+  direction: "vertical",
+  slidesPerView: 1,
+  mousewheel: true,
+    speed: 600,
+      parallax: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
+});
