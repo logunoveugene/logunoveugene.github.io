a54c140143ded572ac7f20bca7c8c8aabedec6c5
logunoveugene <logunov.design@yandex.ru>
2018-07-16 22:18:42 +1000

    1

--- home3/js/custom.js
+++ home3/js/custom.js
@@ -1,69 +1,77 @@
 
 
-    var config = {
-    apiKey: "AIzaSyCMbnQPl_26yGxXdElx-Vx1ZGkoC8vTsZI",
-    authDomain: "homepage-fed4c.firebaseapp.com",
-    databaseURL: "https://homepage-fed4c.firebaseio.com"
-    }
+var config = {
+  apiKey: "AIzaSyCMbnQPl_26yGxXdElx-Vx1ZGkoC8vTsZI",
+  authDomain: "homepage-fed4c.firebaseapp.com",
+  databaseURL: "https://homepage-fed4c.firebaseio.com"
+}
 
 
 
-    firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-    var mainNavsRef = firebase.database().ref('mainNavs');
-    var mainbannersRef = firebase.database().ref('mainbanners');
-    var productsmRef = firebase.database().ref('products');
-    var setsRef = firebase.database().ref('sets');
+var mainNavsRef = firebase.database().ref('mainNavs');
+var mainbannersRef = firebase.database().ref('mainbanners');
+var productsmRef = firebase.database().ref('products');
+var setsRef = firebase.database().ref('sets');
 
 
-    Vue.config.devtools = true;
+Vue.config.devtools = true;
 
-    Vue.use(VueAwesomeSwiper)
+Vue.use(VueAwesomeSwiper)
 
-    var main = new Vue({
-      el: '#app',
-      components: {
-        LocalSwiper: VueAwesomeSwiper.swiper,
-        LocalSlide: VueAwesomeSwiper.swiperSlide,
-      },
+var main = new Vue({
+  el: '#app',
+  components: {
+    LocalSwiper: VueAwesomeSwiper.swiper,
+    LocalSlide: VueAwesomeSwiper.swiperSlide,
+  },
 
-      data: function() {
-        return {
-          awesomeSwiperA: {
-            spaceBetween:30,
-            slidesPerView: 'auto',
-            navigation: {
-              nextEl: '.swiper-button-next',
-              prevEl: '.swiper-button-prev',
-            }
-
-          }
+  data: function() {
+    return {
+      mainBannerOption: {
+        centeredSlides: true,
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'fraction'
+        },
+        navigation: {
+          nextEl: '.sw-button-next',
+          prevEl: '.sw-button-prev'
         }
       },
-
-      firebase: {
-        mainNavs: mainNavsRef,
-        mainbanners: mainbannersRef,
-        products: productsmRef,
-        sets: setsRef
-      },
-
-      filters: {
-        text180:function(value) {
-          var sliced = value.slice(0,170);
-          if (sliced.length < value.length) {
-            return sliced += '...';
-          }
-          return value;
+      setsOption: {
+slidesPerView: 3,
+          spaceBetween: 30,      
+        navigation: {
+          nextEl: '.sw-button-next',
+          prevEl: '.sw-button-prev'
         }
-      },
-      
-      computed: {
-      },
-
-      methods: {
       }
-    });
+    }
+  },
+
+  firebase: {
+    mainNavs: mainNavsRef,
+    mainbanners: mainbannersRef,
+    products: productsmRef,
+    sets: setsRef
+  },
+
+  filters: {
+    text180:function(value) {
+      var sliced = value.slice(0,170);
+      if (sliced.length < value.length) {
+        return sliced += '...';
+      }
+      return value;
+    }
+  },
+
+
+  methods: {
+  }
+});
 
 
 
