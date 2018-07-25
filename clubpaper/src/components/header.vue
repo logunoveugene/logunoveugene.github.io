47dbab8f686949d8d98e5430747e48e2e5ac1434
eugene <logunovevg@Mac-mini-Pavel-2.local>
2018-07-25 17:16:01 +1000

    1

--- clubpaper/src/components/header.vue
+++ clubpaper/src/components/header.vue
@@ -73,12 +73,64 @@
 				</div>
 			</div>
 		</div>
+
+
+		<fixed-header class="main-layout" :class="live" :threshold="60" :fixed.sync="isFixed">
+			<div class="fixed-plate" :class="{ 'is-fixed': isFixed }">
+				<div class="header__fixed-bottom-line">
+					<div class="header__container container">
+						<div class="header__bottom-grid row">
+							<div class="header__bottom-block col-2 d-lg-none">
+								<div class="header__mobile-menu">
+									<img src="https://i.snag.gy/23I0JN.jpg" alt="">
+								</div>
+							</div>
+							<div class="header__bottom-block col-8 col-lg-8">
+								<img class="heaader__logo" src="https://i.snag.gy/p4txCj.jpg" alt="" > 
+								<div class="header__main-nav d-none d-lg-flex">
+									<a href="#" class="link link--color-grey">Дайджест</a> 
+									<a href="#" class="link link--color-grey">Обзоры</a> 
+									<a href="#" class="link link--color-grey">Лайфхаки</a>
+									<a href="#" class="link link--color-grey ">Обсуждения</a>
+								</div>
+							</div>
+							<div class="header__bottom-block col-2 col-lg-4">
+								<div class="header__create-controls d-none d-lg-flex">
+									<div class="header__create-controls-item">
+										<div class="header__create-controls-icon">
+											<div class="icon-pen"></div>
+										</div>
+										<a href="#" class="link link--color-grey">Написать</a>
+									</div>
+									<div class="header__create-controls-item">
+										<div class="header__create-controls-icon">
+											<div class="icon-ask"></div>
+										</div>
+										<a href="#" class="link link--color-grey">Спросить</a>
+									</div>
+								</div>
+								<div class="header__search">
+									<img src="https://i.snag.gy/7rBCoa.jpg" alt="">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</fixed-header>
+		
 	</div>
 
 </template>
 
 <script>
+
+	import FixedHeader from 'vue-fixed-header'
+
 	export default {
+		components: {
+			FixedHeader
+		},
 		props: { 
 			live: {
 				type: null,
@@ -87,13 +139,34 @@
 		},
 		data: function() {
 			return {
-
+				isFixed: false
 			}
 		}
 	}
 </script>
 
 <style>
+
+
+.fixed-plate{
+	position: fixed;
+
+display: none;
+}
+.fixed-plate.is-fixed{
+	position: fixed;
+	top:0;
+
+	z-index: 100;
+	width: 100%;
+	background-color: #fff;
+	display: block;
+}
+.header__fixed-bottom-line{
+	padding: 1rem 0;
+	box-shadow: 0 0 15px rgba(0,0,0,.1);
+}
+
 .header__top-line {
 	width: auto;
 	background-color: #f6f6f6;
@@ -158,8 +231,8 @@
 }
 
 .header__bottom-grid {
-    display: flex;
-    align-items: center;
+	display: flex;
+	align-items: center;
 }
 
 
