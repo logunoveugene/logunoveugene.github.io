f49da9e21074254d33986f24bdb961acb82723e1
logunoveugene <logunov.design@yandex.ru>
2018-08-18 18:26:28 +1000

    1

---
+++ next/club/pages/post/index.vue
@@ -0,0 +1,20 @@
+<template>
+	<ul>
+		<li  v-for="(post, index) in posts" :key="index">{{post.title}}
+				<nuxt-link :to="{path:'/post/'+ post.id }">ьщку</nuxt-link>	
+		</li>
+	</ul>
+</template>
+
+<script>
+import fetch from 'isomorphic-fetch'
+
+export default {
+async asyncData() {
+	const responce =  await fetch('https://club-paper.firebaseio.com/digest.json?orderBy="$key"&limitToLast=3');
+	const posts = await responce.json();
+	return { posts };
+}
+}
+</script>
+
