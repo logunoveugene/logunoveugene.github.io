ab1855f1209e74203e6bd785753a51bbe86efc92
logunoveugene <logunov.design@yandex.ru>
2018-09-22 09:23:17 +1000

    1

--- dns/src/components/best-product.vue
+++ dns/src/components/best-product.vue
@@ -24,6 +24,7 @@
 		:slideinfo="slide[curentIndex] "
 		></product-slider>
 	</keep-alive>
+
 </div>
 </template>
 
@@ -48,8 +49,6 @@
 				default: function () {
 					return ['label', 'value']
 				}
-
-
 			}
 		},
 
