98beb40cd632233b6bdb4a35e2378867117c9655
logunoveugene <logunov.design@yandex.ru>
2018-11-15 22:27:27 +1000

    1

---
+++ storybook/.storybook/config.js
@@ -0,0 +1,46 @@
+import { configure } from '@storybook/html';
+import { configureViewport } from '@storybook/addon-viewport';
+
+
+const newViewports = {
+    responsive:{
+        name: 'Responsive',
+        styles: {
+            width: '100%',
+            height: '100%',
+        },
+        type: 'desktop',
+    },
+    phone: {
+        name: 'Phone(320)',
+        styles: {
+            width: '320px',
+            height: '568px'
+        }
+    },
+    tablet: {
+        name: 'Tablet',
+        styles: {
+            width: '768px',
+            height: '1024px'
+        }
+    },
+    desktop: {
+        name: 'Desktop',
+        styles: {
+            width: '1024px',
+            height: '768px'
+        }
+    },
+};
+
+configureViewport({
+    viewports: newViewports
+});
+
+function loadStories() {
+    require('../stories/index.js');
+    // You can require as many stories as you need.
+}
+
+configure(loadStories, module);
---
+++ storybook/.storybook/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                loaders: ["style-loader", "css-loader", "sass-loader"],
+                include: path.resolve(__dirname, "../")
+            },
+            {
+                test: /\.js?$/,
+                loaders: [require.resolve('@storybook/addon-storysource/loader')],
+                include: path.resolve(__dirname, '../src'),
+                enforce: 'pre',
+            },
+        ]
+    }
+};
