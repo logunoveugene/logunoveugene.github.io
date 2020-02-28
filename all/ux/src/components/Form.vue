2cfa9846f2f40c68a00d60f5b3073ae0c826bbc5
logunoveugene <logunov.design@yandex.ru>
2020-02-28 14:02:38 +1000

    sdf

--- all/ux/src/components/Form.vue
+++ all/ux/src/components/Form.vue
@@ -65,7 +65,7 @@
 			issueReady() {
 				let userAgent = window.navigator.userAgent;
 
-				window.ga('send', 'event', 'Videos', '2020-02-27-test-ux', this.answer[0] +  ' , '+ this.answer[1] +  ' , '+ this.answer[2] +  ' , '+ this.answer[3] +  ' , '+ this.answer[4] +  ' , '+ this.answer[5] +' , '+  userAgent);
+				window.gtag('event', 'click', { 'event_category':'2020-02-27-test-ux', 'event_label': this.answer[0] +  ' , '+ this.answer[1] +  ' , '+ this.answer[2] +  ' , '+ this.answer[3] +  ' , '+ this.answer[4] +  ' , '+ this.answer[5] +' , '+  userAgent});
 				window.ym(52268173, 'reachGoal', '2020-02-27-test-ux', {'2020-02-27-test-ux': this.answer[0] +  ' , '+ this.answer[1] +  ' , '+ this.answer[2] +  ' , '+ this.answer[3] +  ' , '+ this.answer[4] +  ' , '+ this.answer[5] +' , '+  userAgent });
 				alert("спасибо за участие");
 				},
